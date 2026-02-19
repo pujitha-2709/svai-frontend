603240c011e6d6bc47517a5da5b819d297244430
Pujitha <pujithareddy2709@gmail.com>
2026-02-19 23:37:04 +0530

    Fix MySQL connection using MYSQL_URL

--- db.js
+++ db.js
@@ -8,7 +8,12 @@ const __dirname = path.dirname(__filename);
 
 dotenv.config({ path: path.resolve(__dirname, '.env') });
 
+import mysql from 'mysql2/promise';
+
 const pool = mysql.createPool(process.env.MYSQL_URL);
+
+export { pool };
+
 
 
 async function query(sql, params) {
